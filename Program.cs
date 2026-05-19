e1be46a6f39ab525bfb4ae82ee9733c060b4c073
RicardoFrancoDelgado <rf041555@gmail.com>
2026-05-19 00:42:51 -0300

    feat: Adicionando operações matemáticas e finalizando o menu de interação da calculadora

--- Program.cs
+++ Program.cs
@@ -6,7 +6,106 @@ namespace Calculadora
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Menu();
+        }
+
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("Iniciando calculadora...");
+            Console.WriteLine("Escolha uma das operações a seguir");
+            Console.WriteLine("0 - Sair");
+            Console.WriteLine("1 - Soma");
+            Console.WriteLine("2 - Subtração");
+            Console.WriteLine("3 - Divisão");
+            Console.WriteLine("4 - Multiplicação");
+
+            Console.WriteLine("--------------------");
+            Console.Write("Escolha uma opção: ");
+            short res = short.Parse(Console.ReadLine());
+
+            switch (res)
+            {
+                case 0: Sair(); break;
+                case 1: Soma(); break;
+                case 2: Subtracao(); break;
+                case 3: Divisao(); break;
+                case 4: Multiplicacao(); break;
+                default:
+                    Console.WriteLine("Operação não reconhecida");
+                    Menu();
+                    break;
+            }
+        }
+
+        static void Sair()
+        {
+            Console.WriteLine("Encerrando calculadora...");
+            System.Environment.Exit(0);
+        }
+
+        static void Soma()
+        {
+            Console.Clear();
+            Console.WriteLine("Informe o primeiro número:");
+            double v1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Informe o segundo número:");
+            double v2 = double.Parse(Console.ReadLine());
+
+            double res = v1 + v2;
+            Console.WriteLine($"Resultado da soma: {res}");
+
+            Console.ReadKey();
+            Menu();
+        }
+
+        static void Subtracao()
+        {
+            Console.Clear();
+            Console.WriteLine("Informe o primeiro número:");
+            double v1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Informe o segundo número:");
+            double v2 = double.Parse(Console.ReadLine());
+
+            double res = v1 - v2;
+            Console.WriteLine($"Resultado da subtração: {res}");
+
+            Console.ReadKey();
+            Menu();
+        }
+
+        static void Divisao()
+        {
+            Console.Clear();
+            Console.WriteLine("Informe o primeiro número:");
+            double v1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Informe o segundo número:");
+            double v2 = double.Parse(Console.ReadLine());
+
+            double res = v1 / v2;
+            Console.WriteLine($"Resultado da divisão: {res}");
+
+            Console.ReadKey();
+            Menu();
+        }
+
+        static void Multiplicacao()
+        {
+            Console.Clear();
+            Console.WriteLine("Informe o primeiro número:");
+            double v1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Informe o segundo número:");
+            double v2 = double.Parse(Console.ReadLine());
+
+            double res = v1 * v2;
+            Console.WriteLine($"Resultado da multiplicação: {res}");
+
+            Console.ReadKey();
+            Menu();
         }
     }
 }
--- obj/Debug/netcoreapp3.1/Calculadora.AssemblyInfo.cs
+++ obj/Debug/netcoreapp3.1/Calculadora.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculadora")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+466d2689f0e03e83247945c286e4d5db102e7177")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculadora")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculadora")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
